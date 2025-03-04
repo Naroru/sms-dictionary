fae89387343dd761032b84790aa5c24cd5287fc1
naro <obokerov@v6services.ru>
2025-03-04 16:20:23 +0300

    SMS-7: fix dto

--- src/main/java/com/example/sms_dictionary/model/supplier/service/dto/SupplierDto.java
+++ src/main/java/com/example/sms_dictionary/model/supplier/service/dto/SupplierDto.java
@@ -1,6 +1,5 @@
 package com.example.sms_dictionary.model.supplier.service.dto;
 
-import com.example.sms_dictionary.model.common.entity.ContactInfo;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +9,5 @@ public class SupplierDto {
   private Long id;
   private String name;
   private String inn;
-  private ContactInfo contactInfo;
+  private SupplierContactInfoDto contactInfo;
 }
